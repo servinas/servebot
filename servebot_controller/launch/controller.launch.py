48fc6fdea7c6e804d41120fd38aad868f73d682b
Servinas <servinas.hassan@gmail.com>
2025-07-02 17:41:01 +0300

    simple_controllerv1.0

--- servebot_controller/launch/controller.launch.py
+++ servebot_controller/launch/controller.launch.py
@@ -1,8 +1,25 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
 
 
 def generate_launch_description():
+    
+    use_python_arg = DeclareLaunchArgument(
+        "use_python",
+        default_value="True"
+    )
+    
+    wheel_radius_arg = DeclareLaunchArgument(
+        "wheel_radius",
+        default_value="0.033",
+        description="Radius of the wheels.",
+    )
+    wheel_separation_arg = DeclareLaunchArgument(
+        "wheel_separation",
+        default_value="0.17",
+        description="Distance between the wheels.",
+    )
     
     joint_state_boadcaster_spawner=Node(
         package="controller_manager",
