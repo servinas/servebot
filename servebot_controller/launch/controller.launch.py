3913ce682b01f41d58020e034d64c7230d7f79d9
Servinas <servinas.hassan@gmail.com>
2025-07-03 02:15:50 +0300

    servebot_control usig joy stick

--- servebot_controller/launch/controller.launch.py
+++ servebot_controller/launch/controller.launch.py
@@ -1,7 +1,8 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
-
+from launch.actions import DeclareLaunchArgument , GroupAction
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition , UnlessCondition
 
 def generate_launch_description():
     
@@ -20,7 +21,19 @@ def generate_launch_description():
         default_value="0.17",
         description="Distance between the wheels.",
     )
+    use_simple_controller_arg = DeclareLaunchArgument(
+        "use_simple_controller",
+        default_value="True"
+    )
     
+    
+    use_python = LaunchConfiguration("use_python")
+    wheel_radius = LaunchConfiguration("wheel_radius")
+    wheel_separation = LaunchConfiguration("wheel_separation")
+    use_simple_controller = LaunchConfiguration("use_simple_controller")
+    
+    
+        
     joint_state_boadcaster_spawner=Node(
         package="controller_manager",
         executable="spawner",
@@ -30,17 +43,50 @@ def generate_launch_description():
             "/controller_manager"
         ]
     )
-    simple_controller=Node(
+    
+    wheel_controller_spawner=Node(
         package="controller_manager",
         executable="spawner",
         arguments=[
-            "simple_velocity_controller",
+            "servebot_controller",
             "--controller-manager",
             "/controller_manager"
+        ],
+        condition = UnlessCondition(use_simple_controller)
+        )
+    simple_controller=GroupAction(
+        condition= IfCondition(use_simple_controller),
+        actions=[
+            Node(
+                package="controller_manager",
+                executable="spawner",
+                arguments=[
+                    "simple_velocity_controller",
+                    "--controller-manager",
+                    "/controller_manager"
+                ]
+                
+            ),
+    
+    
+            Node(
+                package="servebot_controller",
+                executable="simple_controller.py",
+                parameters=[{"wheel_radius":wheel_radius,
+                            "wheel_separation":wheel_separation}],
+                condition=IfCondition(use_python)
+            )
         ]
     )
+   
     
     return LaunchDescription([
+        use_python_arg,
+        wheel_radius_arg,
+        wheel_separation_arg,
+        use_simple_controller_arg,
         joint_state_boadcaster_spawner,
-        simple_controller
+        wheel_controller_spawner,
+        simple_controller,
+        
     ])
